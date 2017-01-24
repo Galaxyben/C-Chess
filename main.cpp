cb627f0cf7712cac7862a6b7482ce5f944be5150
Adan Sandoval <adansinacento@icloud.com>
2017-01-24 08:43:44 -0600

    Creation of main.cpp
    Aun falta trabajar en este documento

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+char p1Positions[8][8];
+char p2Positions[8][8];
+
+void llenarArreglos(){
+    for (int i = 0; i<8; i++)
+    {
+        p1Positions[0][1] = 'P';
+    }
+}
+
+void printBoard(){
+    cout << "  __ __ __ __ __ __ __ __" << endl;
+    for (int i = 0; i<8; i++)
+    {
+        cout << " ";
+        for (int j = 0; j<8; j++)
+        {
+            cout << "|  ";
+        }
+        cout << "|" << endl << (8-i);
+        for (int j = 0; j<8; j++)
+        {
+            cout << "|__";
+        }
+        cout << "|" << endl;
+
+    }
+    cout << "   A  B  C  D  E  F  G  H" << endl;
+
+}
+
+int main()
+{
+    printBoard();
+    return 0;
+}
