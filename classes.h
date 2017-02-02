dfbc5522548c4cd4f7c005d48a691e5b39a7132b
Galaxyben <galaxyben1@gmail.com>
2017-02-02 16:42:10 -0600

    Update classes.h

--- classes.h
+++ classes.h
@@ -104,3 +104,244 @@ void Peon::checkMoves(Players p1, Players p2)
         }
     }
 }
+class Torre: public piezas {
+        public
+            void checkMoves (Players p1, Players p2);
+};
+void Torre::checkMoves(Players p1, Players p2)
+{
+    if (color == 'B')
+    {
+        cout << "Tus movimientos disponibles son: " << endl;
+
+        int i=1
+
+        while (i != 0 )
+        {
+            if (p2.positions[x+j][y] == NULL)
+            {
+                cout << " " << (8-(x+j)) << char(65+(y));
+            }
+            if (p2.positions [x-j][y] == NULL)
+            {
+                cout << " " << (8-(x-j)) << char(65+(y));
+            }
+            i++
+            else
+            {
+                i=0
+            }
+
+        }
+        while (i= 0)
+        {
+            if (p2.positions [x][y+j] == NULL)
+            {
+                cout << " " << (8-(x)) << char(65+(y+j));
+            }
+            if (p2.positions [x][y-j] == NULL)
+            {
+                cout << " " << (8-(x)) << char(65+(y-j);
+            }
+            i++
+            else
+            {
+                i=0
+            }
+        }
+ ////////////////////////////////SIGUIENTE CLASE////////////////////////////
+ class Caballo: public piezas {
+        public
+            void checkMoves (Players p1, Players p2);
+};
+void Caballo::checkMoves(Players p1, Players p2)
+{
+    if (color == 'B')
+    {
+        cout << "Tus movimientos disponibles son: " << endl;
+        int i = 1
+
+        while (i != 0)
+        {
+            if (p2.positions [x+2][y+1] == NULL)
+            {
+                cout << " " << (8-(x+2)) << char(65+(y+1));
+            }
+            if (p2.positions [x+1][y+2] == NULL)
+            {
+                cout << " " << (8-(x+1)) << char(65+(y+2));
+            }
+            if (p2.positions [x+2][y-1] == NULL)
+            {
+                cout << " " << (8-(x+2)) << char (65+(y-1));
+            }
+            if (p2.positions ([x+1][y-2]) << == NULL)
+            {
+                cout << " " << (8-(x+1)) << char (65+(y-2));
+            }
+            if (p2.positions ([x-1][y+2]) == NULL)
+            {
+                cout << " " << (8-(x-1)) << char (65+(y+2));
+            }
+            if (p2.positions ([x-2][y-1]) == NULL)
+            {
+                cout << " " << (8-(x-2)) << char (65+(y-1));
+            }
+            if (p2.positions ([x-2][y+1]) == NULL)
+            {
+                cout << " " << (8-(x-2)) << char (65+(y+1));
+            }
+            if (p2.positions ([x-1][y+2]) == NULL)
+            {
+                cout << " " << (8-(x-1)) << char (65+(y+2));
+            }
+        }
+
+
+    }
+
+ ////////////////////////////////SIGUIENTE CLASE////////////////////////////
+  class Alfil: public piezas {
+        public
+            void checkMoves (Players p1, Players p2);
+};
+void Alfil::checkMoves(Players p1, Players p2)
+{
+    if (color == 'B')
+    {
+        cout << "Tus movimientos disponibles son: " << endl;
+
+        int i = 1
+        
+        while (i != 0 )
+        {
+            if (p2.positions[x+j][y+j] == NULL)
+            {
+                cout << " " << (8-(x+j)) << char(65+(y+j));
+            }
+            if (p2.positions [x-j][y-j] == NULL)
+            {
+                cout << " " << (8-(x-j)) << char(65+(y-j));
+            }
+            if (p2.positions[x+j][y-j] == NULL)
+            {
+                cout << " " << (8-(x+j)) << char(65+(y-j));
+            }
+            if (p2.positions [x-j][y+j] == NULL)
+            {
+                cout << " " << (8-(x-j)) << char(65+(y+j));
+            }
+            if (p2.positions [x][y+j] == NULL)
+            {
+                cout << " " << (8-(x)) << char(65+(y+j));
+            }
+            if (p2.positions [x][y-j] == NULL)
+            {
+                cout << " " << (8-(x)) << char(65+(y-j);
+            }
+            if (p2.positions [x+2][y+1] == NULL)
+            {
+                cout << " " << (8-(x+2)) << char(65+(y+1));
+            }
+            if (p2.positions [x+1][y+2] == NULL)
+            {
+                cout << " " << (8-(x+1)) << char(65+(y+2));
+            }
+            if (p2.positions [x+2][y-1] == NULL)
+            {
+                cout << " " << (8-(x+2)) << char (65+(y-1));
+            }
+            if (p2.positions ([x+1][y-2]) << == NULL)
+            {
+                cout << " " << (8-(x+1)) << char (65+(y-2));
+            }
+            if (p2.positions ([x-1][y+2]) == NULL)
+            {
+                cout << " " << (8-(x-1)) << char (65+(y+2));
+            }
+            if (p2.positions ([x-2][y-1]) == NULL)
+            {
+                cout << " " << (8-(x-2)) << char (65+(y-1));
+            }
+            if (p2.positions ([x-2][y+1]) == NULL)
+            {
+                cout << " " << (8-(x-2)) << char (65+(y+1));
+            }
+            if (p2.positions ([x-1][y+2]) == NULL)
+            {
+                cout << " " << (8-(x-1)) << char (65+(y+2));
+            }
+            i++
+            else
+            {
+                i=0
+            }
+        }
+        }
+
+ ////////////////////////////////SIGUIENTE CLASE////////////////////////////
+   class Reina: public piezas {
+        public
+            void checkMoves (Players p1, Players p2);
+};
+void Reina::checkMoves(Players p1, Players p2)
+{
+    if (color == 'B')
+    {
+        cout << "Tus movimientos disponibles son: " << endl;
+
+        int i = 1
+        
+        while (i != 0 )
+        {
+            if (p2.positions[x+j][y+j] == NULL)
+            {
+                cout << " " << (8-(x+j)) << char(65+(y+j));
+            }
+            if (p2.positions [x-j][y-j] == NULL)
+            {
+                cout << " " << (8-(x-j)) << char(65+(y-j));
+            }
+            if (p2.positions[x+j][y-j] == NULL)
+            {
+                cout << " " << (8-(x+j)) << char(65+(y-j));
+            }
+            if (p2.positions [x-j][y+j] == NULL)
+            {
+                cout << " " << (8-(x-j)) << char(65+(y+j));
+            }
+            i++
+            else
+            {
+                i=0
+            }
+
+        }
+        while (i= 0)
+        {
+            if (p2.positions [x][y+j] == NULL)
+            {
+                cout << " " << (8-(y+j)) << char(65+(x));
+            }
+            if (p2.positions [x][y-j] == NULL)
+            {
+                cout << " " << (8-(y-j)) << char(65+(x);
+            }
+            i++
+            else
+            {
+                i=0
+            }
+        }
+        }
+
+ ////////////////////////////////SIGUIENTE CLASE////////////////////////////
+    class Rey: public piezas {
+        public
+            void checkMoves (Players p1, Players p2);
+};
+void Rey::checkMoves(Players p1, Players p2)
+{
+    if (color == 'B')
+    {
+        cout << "Tus movimientos disponibles son: " << endl;
